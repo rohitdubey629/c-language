fd14b060f185322945c5c302f53286bfaf8a7bbf
Rohit .bca <dubeyrohit629@gmail.com>
2022-01-31 20:57:54 +0530

    Rename File

--- WHILE1HE.C
+++ WHILE1HE.C
@@ -1,13 +1,16 @@
-main(){
- int i=1,t,num;
- clrscr();
- printf("Enter the number");
- scanf("%d",&num);
- while(i<=10)
- {
- t=num*i;
-  printf("%d\n",t);
-  i++;
- }
- getch();
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+    int i = 1, t, num;
+    clrscr();
+    printf("Enter the number");
+    scanf("%d", &num);
+    while (i <= 10)
+    {
+        t = num * i;
+        printf("%d\n", t);
+        i++;
+    }
+    getch();
 }
